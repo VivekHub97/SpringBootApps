90a7389a22d4a3e7a1eb915c250c98469343fa88
Vivek Agarwal <agarwalvivek.1997@gmail.com>
2024-11-10 22:47:39 +0100

    Project JobListing initialized (Boot MongoDB)

---
+++ JobListingBootMongoApp/src/main/java/com/vivekprojects/JobListingBootMongoApp/JobListingBootMongoAppApplication.java
@@ -0,0 +1,13 @@
+package com.vivekprojects.JobListingBootMongoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobListingBootMongoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobListingBootMongoAppApplication.class, args);
+	}
+
+}
---
+++ JobListingBootMongoApp/src/test/java/com/vivekprojects/JobListingBootMongoApp/JobListingBootMongoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivekprojects.JobListingBootMongoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobListingBootMongoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
