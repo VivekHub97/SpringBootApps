fd92fc97bba500a8cce586f9280af74e0122b291
Vivek Agarwal <agarwalvivek.1997@gmail.com>
2024-11-11 01:25:21 +0100

    Added method to post a joblisting

--- JobListingBootMongoApp/src/main/java/com/vivekprojects/JobListingBootMongoApp/controller/PostController.java
+++ JobListingBootMongoApp/src/main/java/com/vivekprojects/JobListingBootMongoApp/controller/PostController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +35,11 @@ public class PostController {
 		
 		return repo.findAll();
 		
+	}
+	
+	@PostMapping("/post")
+	public Post addPost(@RequestBody Post post) {
+		
+		return repo.save(post);
 	}
 }
