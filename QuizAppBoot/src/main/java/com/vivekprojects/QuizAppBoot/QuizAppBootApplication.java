123edff79df5b0c03062bf59193c6756d0dffe2e
Vivek Agarwal <agarwalvivek.1997@gmail.com>
2024-11-12 22:54:18 +0100

    Initiated new Web Boot App for Quiz. Initially there will monolithic architecture (Initializr)(Boot, Web, Jpa, PostGres)

---
+++ QuizAppBoot/src/main/java/com/vivekprojects/QuizAppBoot/QuizAppBootApplication.java
@@ -0,0 +1,13 @@
+package com.vivekprojects.QuizAppBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAppBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizAppBootApplication.class, args);
+	}
+
+}
---
+++ QuizAppBoot/src/test/java/com/vivekprojects/QuizAppBoot/QuizAppBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivekprojects.QuizAppBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
