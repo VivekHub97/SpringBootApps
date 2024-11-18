7b87c54dc59e0d6b5b67f50ceaf6a660d0f6c24b
Vivek Agarwal <agarwalvivek.1997@gmail.com>
2024-11-18 01:54:11 +0100

    Adding response Entity to get http status code

--- QuizAppBoot/src/main/java/com/vivekprojects/QuizAppBoot/controller/QuestionController.java
+++ QuizAppBoot/src/main/java/com/vivekprojects/QuizAppBoot/controller/QuestionController.java
@@ -3,6 +3,8 @@ package com.vivekprojects.QuizAppBoot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,19 +22,25 @@ public class QuestionController {
 	@Autowired
 	QuestionService questionService;
 	
+//	@GetMapping("allQuestions")
+//	public List<Question> getAllQuestions() {
+//		return questionService.getAllQuestions();
+//		
+//	}
+	
 	@GetMapping("allQuestions")
-	public List<Question> getAllQuestions() {
+	public ResponseEntity<List<Question>> getAllQuestions() {
 		return questionService.getAllQuestions();
 		
 	}
 	
 	@GetMapping("category/{category}")
-	public List<Question> getQuestionsByCategory(@PathVariable String category){		
+	public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category){		
 		return questionService.getQuestionsByCategory(category);
 	}
 	
 	@PostMapping("add")
-	public String addQuestion(@RequestBody Question question){		
+	public ResponseEntity<String> addQuestion(@RequestBody Question question){		
 		return questionService.addQuestion(question);
 	}
 
--- QuizAppBoot/src/main/java/com/vivekprojects/QuizAppBoot/service/QuestionService.java
+++ QuizAppBoot/src/main/java/com/vivekprojects/QuizAppBoot/service/QuestionService.java
@@ -1,8 +1,11 @@
 package com.vivekprojects.QuizAppBoot.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.vivekprojects.QuizAppBoot.Question;
@@ -13,21 +16,37 @@ public class QuestionService {
 
 	@Autowired
 	QuestionDao questionDao;
-	
-	public List<Question> getAllQuestions() {
+
+//	public List<Question> getAllQuestions() {
+//		// TODO Auto-generated method stub
+//		return questionDao.findAll();
+//	}
+
+	public ResponseEntity<List<Question>> getAllQuestions() {
 		// TODO Auto-generated method stub
-		return questionDao.findAll();
+		try {
+			return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
 	}
 
-	public List<Question> getQuestionsByCategory(String category) {
+	public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
 		// TODO Auto-generated method stub
-		return questionDao.findByCategory(category);
+		try {
+			return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
 	}
 
-	public String addQuestion(Question question) {
+	public ResponseEntity<String> addQuestion(Question question) {
 		// TODO Auto-generated method stub
 		questionDao.save(question);
-		return "Question Added";
+		return new ResponseEntity<>("Question Added", HttpStatus.CREATED);
 	}
 
 }
